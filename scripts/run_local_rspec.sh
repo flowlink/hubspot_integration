6d1adfe20479418395cb6566cc33f7f943ddb72a
Marc Salo <marc@nurelm.com>
2019-11-27 14:31:32 -0500

    fixed typo

--- scripts/run_local_rspec.sh
+++ scripts/run_local_rspec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml \
                -f docker-compose.dev.yml \
-               run --rm -e RAILS_ENV=test shipstation-integration \
+               run --rm -e RAILS_ENV=test hubspot-integration \
                sh -c "bundle exec rspec ${@}"
